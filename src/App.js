6b23f91fccdd2dc62443bda27942cffd529b853b
JIRATEEP JANKHAEW <6210110045@psu.ac.th>
2022-10-12 17:49:04 +0700

    edit position item

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import { Link } from "react-router-dom";
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1>Home page</h1>
-        <Link to="/profile">Go back to profile</Link>
         <div>
-          <img 
-            src="https://play-lh.googleusercontent.com/5rS-un965hkZuqUNA65RwkeqmGko91DVQHlAMCQ9lJlNguvXkCQzjl6OgAMOPZ2cCk4" 
+          <img
+            src="https://play-lh.googleusercontent.com/5rS-un965hkZuqUNA65RwkeqmGko91DVQHlAMCQ9lJlNguvXkCQzjl6OgAMOPZ2cCk4"
             alt="Home"
           />
-        </div>
+        </div><br/>
+        <Link to="/profile">Go back to profile</Link>
       </div>
     );
   }
--- src/Profile.js
+++ src/Profile.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
- 
+
 export default class Profile extends React.Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <h1>This is my profile</h1>
-        <Link to="/">Go back to home</Link>
         <div>
-          <img 
-            src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80" 
+          <img
+            src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
             alt="Profile"
           />
-        </div>
+        </div><br/>
+        <Link to="/">Go back to home</Link>
       </div>
     );
   }
